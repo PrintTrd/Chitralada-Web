1bb81bffbfc9a3e61fa6521abfaf6850d44bd331
PrintTrd <60070034@kmitl.ac.th>
2019-11-03 16:28:14 +0700

    showFirstDetail

---
+++ product.js
@@ -0,0 +1,21 @@
+// nowIndex = 0;
+// title = [
+// "ไอศกรีมนมสดหนองโพ"
+// ];
+// detail1 = [
+// "รสชาติ: รสนม, รสวนิลา, รสช็อกโกแลต, รสสตรอว์เบอร์รี"
+// ];
+// detail2 = [
+// "ปริมาณสุทธิ: 85 กรัม"
+// ];
+// detail3 = [
+// "ราคา: 35 บาท"
+// ];
+function show(){
+    detailbox.classList.remove("hide");
+    detailbox.classList.add("show");
+}
+function hide(){
+    detailbox.classList.remove("show");
+    detailbox.classList.add("hide");
+}
