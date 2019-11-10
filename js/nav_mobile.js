090b86051f25e021a01d6be763fa4fecee58fa47
PrintTrd <60070034@kmitl.ac.th>
2019-11-10 11:11:27 +0700

    contact page

--- js/nav_mobile.js
+++ js/nav_mobile.js
@@ -2,7 +2,9 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
+        document.getElementById("hamburger").innerHTML = "X";
     } else {
         x.className = "topnav";
+        document.getElementById("hamburger").innerHTML = "&#9776;";
     }
 }
